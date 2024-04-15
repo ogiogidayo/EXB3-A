8054560d0e19c7596c327e6be7b0295b75762f09
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-04-15 13:56:36 +0900

    feat: add method

--- src/main/java/labo/Labo2Task01.java
+++ src/main/java/labo/Labo2Task01.java
@@ -1,11 +1,38 @@
 package labo;
 
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Labo2Task01 {
     public static void main(String[] args) {
         String file = getFileName();
-        System.out.println(file);
+        int i = 0;
+        SnowRecord[] records=new SnowRecord[10000];
+        try (Scanner sc_f = new Scanner(Path.of(file), "utf8")) {
+            if (sc_f.hasNextLine()) {
+                sc_f.nextLine();  // 最初の行を読み飛ばす
+            }
+            while (sc_f.hasNext()) {
+                String date = sc_f.next();
+                int amount = sc_f.nextInt();
+                records[i] = new SnowRecord(date, amount);
+                i += 1;
+                if (sc_f.hasNextLine()) {
+                    sc_f.nextLine();
+                }
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        for (SnowRecord record: records){
+            if (record == null){
+                break;
+            }
+            if (!record.match(2018, 1)){
+                continue;
+            }
+            System.out.println(record.toString());
+        }
     }
     public static String getFileName(){
         Scanner sc = new Scanner(System.in);
